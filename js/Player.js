9e8b4b90635414fb69621550c5ffbcd12dc67138
Ama Sánchez <asanchez128@users.noreply.github.com>
2021-04-26 19:30:24 -0500

    Change free camera

--- js/Player.js
+++ js/Player.js
@@ -10,11 +10,11 @@ Player = function(game, canvas) {
 Player.prototype = {
     _initCamera : function(scene, canvas) {
         // On crée la caméra
-        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(0, 5, -10), scene);
-
+        this.camera = new BABYLON.FreeCamera("camera", new BABYLON.Vector3(-20, 5, 0), scene);
+        
         // On demande à la caméra de regarder au point zéro de la scène
         this.camera.setTarget(BABYLON.Vector3.Zero());
-
+    
         // On affecte le mouvement de la caméra au canvas
         this.camera.attachControl(canvas, true);
     }
