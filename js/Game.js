f52776658bafbbeef80fc543385efd2bff5c31c0
Ama Sánchez <asanchez128@users.noreply.github.com>
2021-04-29 10:25:46 -0500

    Add models

--- js/Game.js
+++ js/Game.js
@@ -1,49 +1,132 @@
-Game = function(canvasId) {
-    // Canvas et engine défini ici
-    var canvas = document.getElementById(canvasId);
-    var engine = new BABYLON.Engine(canvas, true);
-    var _this = this;
-    _this.actualTime = Date.now();
-    // On initie la scène avec une fonction associée à l'objet Game
-    this.scene = this._initScene(engine);
-    
-    var _player = new Player(_this, canvas);
+Game = function (canvasId) {
+  // Canvas et engine défini ici
+  var canvas = document.getElementById(canvasId);
+  var engine = new BABYLON.Engine(canvas, true);
+  var _this = this;
+  _this.actualTime = Date.now();
+  // On initie la scène avec une fonction associée à l'objet Game
+  this.scene = this._initScene(engine);
 
-    var _arena = new Arena(_this);
+  var _player = new Player(_this, canvas);
 
-    // Permet au jeu de tourner
-engine.runRenderLoop(function () {
+  var _arena = new Arena(_this);
 
+  this._PlayerData = _player;
+
+  // Les roquettes générées dans Player.js
+  this._rockets = [];
+
+  // Les explosions qui découlent des roquettes
+  this._explosionRadius = [];
+
+  // Permet au jeu de tourner
+  engine.runRenderLoop(function () {
     // Récuperet le ratio par les fps
-    _this.fps = Math.round(1000/engine.getDeltaTime());
+    _this.fps = Math.round(1000 / engine.getDeltaTime());
 
     // Checker le mouvement du joueur en lui envoyant le ratio de déplacement
     //_player._checkMove((_this.fps)/60);
+    // On apelle nos deux fonctions de calcul pour les roquettes
+    _this.renderRockets();
+    _this.renderExplosionRadius();
 
     _this.scene.render();
-    
+
     // Si launchBullets est a true, on tire
-    if(_player.camera.weapons.launchBullets === true){
-        _player.camera.weapons.launchFire();
+    if (_player.camera.weapons.launchBullets === true) {
+      _player.camera.weapons.launchFire();
     }
-});
+  });
 
-    // Ajuste la vue 3D si la fenetre est agrandie ou diminuée
-    window.addEventListener("resize", function () {
-        if (engine) {
-            engine.resize();
-        }
-    },false);
-
+  // Ajuste la vue 3D si la fenetre est agrandie ou diminuée
+  window.addEventListener(
+    "resize",
+    function () {
+      if (engine) {
+        engine.resize();
+      }
+    },
+    false
+  );
 };
 
 Game.prototype = {
-    // Prototype d'initialisation de la scène
-    _initScene : function(engine) {
-        var scene = new BABYLON.Scene(engine);
-        scene.clearColor=new BABYLON.Color3(0,0,0);
-        scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
-        scene.collisionsEnabled = true;
-        return scene;
+  // Prototype d'initialisation de la scène
+  _initScene: function (engine) {
+    var scene = new BABYLON.Scene(engine);
+    scene.clearColor = new BABYLON.Color3(0, 0, 0);
+    scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
+    scene.collisionsEnabled = true;
+    return scene;
+  },
+  renderRockets: function () {
+    for (var i = 0; i < this._rockets.length; i++) {
+      // On crée un rayon qui part de la base de la roquette vers l'avant
+      var rayRocket = new BABYLON.Ray(
+        this._rockets[i].position,
+        this._rockets[i].direction
+      );
+
+      // On regarde quel est le premier objet qu'on touche
+      var meshFound = this._rockets[i].getScene().pickWithRay(rayRocket);
+
+      // Si la distance au premier objet touché est inférieure à 10, on détruit la roquette
+      if (!meshFound || meshFound.distance < 10) {
+        // On vérifie qu'on a bien touché quelque chose
+        if (meshFound.pickedMesh && !meshFound.pickedMesh.isMain) {
+          // On crée une sphere qui représentera la zone d'impact
+          var explosionRadius = BABYLON.Mesh.CreateSphere(
+            "sphere",
+            5.0,
+            20,
+            this.scene
+          );
+          // On positionne la sphère là où il y a eu impact
+          explosionRadius.position = meshFound.pickedPoint;
+          // On fait en sorte que les explosions ne soient pas considérées pour le Ray de la roquette
+          explosionRadius.isPickable = false;
+          // On crée un petit material orange
+          explosionRadius.material = new BABYLON.StandardMaterial(
+            "textureExplosion",
+            this.scene
+          );
+          explosionRadius.material.diffuseColor = new BABYLON.Color3(1, 0.6, 0);
+          explosionRadius.material.specularColor = new BABYLON.Color3(0, 0, 0);
+          explosionRadius.material.alpha = 0.8;
+
+          if (
+            this._PlayerData.isAlive &&
+            this._PlayerData.camera.playerBox &&
+            explosionRadius.intersectsMesh(this._PlayerData.camera.playerBox)
+          ) {
+            // Envoi à la fonction d'affectation des dégâts
+            console.log("hit");
+          }
+
+          this._explosionRadius.push(explosionRadius);
+        }
+        this._rockets[i].dispose();
+        // On enlève de l'array _rockets le mesh numéro i (défini par la boucle)
+        this._rockets.splice(i, 1);
+      } else {
+        let relativeSpeed = 1 / (this.fps / 60);
+        this._rockets[i].translate(
+          new BABYLON.Vector3(0, 0, 1),
+          relativeSpeed,
+          0
+        );
+      }
     }
+  },
+  renderExplosionRadius: function () {
+    if (this._explosionRadius.length > 0) {
+      for (var i = 0; i < this._explosionRadius.length; i++) {
+        this._explosionRadius[i].material.alpha -= 0.02;
+        if (this._explosionRadius[i].material.alpha <= 0) {
+          this._explosionRadius[i].dispose();
+          this._explosionRadius.splice(i, 1);
+        }
+      }
+    }
+  },
 };
--- js/Player.js
+++ js/Player.js
@@ -1,87 +1,96 @@
-Player = function(game, canvas) {
-    // _this est l'accès à la caméraà l'interieur de Player
-    var _this = this;
+Player = function (game, canvas) {
+  // _this est l'accès à la caméraà l'interieur de Player
+  var _this = this;
 
-    // Si le tir est activée ou non
-this.weaponShoot = false;
-this.controlEnabled = true;
+  // Si le tir est activée ou non
+  this.weaponShoot = false;
+  this.controlEnabled = true;
 
-    
+  // Le jeu, chargé dans l'objet Player
+  this.game = game;
 
-    // Le jeu, chargé dans l'objet Player
-    this.game = game;
+  // Initialisation de la caméra
+  this._initCamera(this.game.scene, canvas);
 
+  // On récupère le canvas de la scène
+  var canvas = this.game.scene.getEngine().getRenderingCanvas();
 
-    
-    // Initialisation de la caméra
-    this._initCamera(this.game.scene, canvas); 
-
-    // On récupère le canvas de la scène 
-var canvas = this.game.scene.getEngine().getRenderingCanvas();
-
-// On affecte le clic et on vérifie qu'il est bien utilisé dans la scène (_this.controlEnabled)
-canvas.addEventListener("pointerdown", function(evt) {
-    if (_this.controlEnabled && !_this.weaponShoot) {
+  // On affecte le clic et on vérifie qu'il est bien utilisé dans la scène (_this.controlEnabled)
+  canvas.addEventListener(
+    "pointerdown",
+    function (evt) {
+      if (_this.controlEnabled && !_this.weaponShoot) {
         _this.weaponShoot = true;
         _this.handleUserMouseDown();
-    }
-}, false);
+      }
+    },
+    false
+  );
 
-// On fait pareil quand l'utilisateur relache le clic de la souris
-canvas.addEventListener("pointerup", function(evt) {
-    if (_this.controlEnabled && _this.weaponShoot) {
+  // On fait pareil quand l'utilisateur relache le clic de la souris
+  canvas.addEventListener(
+    "pointerup",
+    function (evt) {
+      if (_this.controlEnabled && _this.weaponShoot) {
         _this.weaponShoot = false;
         _this.handleUserMouseUp();
-    }
-}, false);
-
+      }
+    },
+    false
+  );
 };
 
 Player.prototype = {
-    _initCamera : function(scene, canvas) {
-//         var playerBox = BABYLON.Mesh.CreateBox("headMainPlayer", 3, scene);
-// playerBox.position = new BABYLON.Vector3(-20, 5, 0);
-// playerBox.ellipsoid = new BABYLON.Vector3(2, 2, 2);
-        // On crée la caméra
-        this.camera = new BABYLON.UniversalCamera("camera", new BABYLON.Vector3(-20, 5, 0), scene);
-        // this.camera.playerBox = playerBox;
-        // this.camera.parent = playerBox;
-        this.camera.applyGravity = true;
-        this.camera.checkCollisions = true;
-        this.camera.ellipsoid = new BABYLON.Vector3(1, 1.7, 1);
-        
-        // Axe de mouvement X et Z
-        //this.camera.axisMovement = [false,false,false,false];
-        
-        // Si le joueur est en vie ou non
-        this.isAlive = true;
+  _initCamera: function (scene, canvas) {
+    //         var playerBox = BABYLON.Mesh.CreateBox("headMainPlayer", 3, scene);
+    // playerBox.position = new BABYLON.Vector3(-20, 5, 0);
+    // playerBox.ellipsoid = new BABYLON.Vector3(2, 2, 2);
+    // On crée la caméra
+    this.camera = new BABYLON.UniversalCamera(
+      "camera",
+      new BABYLON.Vector3(-20, 5, 0),
+      scene
+    );
+    // this.camera.playerBox = playerBox;
+    // this.camera.parent = playerBox;
+    this.camera.applyGravity = true;
+    this.camera.checkCollisions = true;
+    this.camera.ellipsoid = new BABYLON.Vector3(1, 1.7, 1);
 
-        // Pour savoir que c'est le joueur principal
-this.camera.isMain = true;
-        
-        // On demande à la caméra de regarder au point zéro de la scène
-        this.camera.setTarget(BABYLON.Vector3.Zero());
-        // This attaches the camera to the canvas
-        this.camera.attachControl(canvas, true);
+    // Axe de mouvement X et Z
+    //this.camera.axisMovement = [false,false,false,false];
 
-        
-        // Appel de la création des armes
+    // Si le joueur est en vie ou non
+    this.isAlive = true;
+
+    // Pour savoir que c'est le joueur principal
+    this.camera.isMain = true;
+    // La santé du joueur
+    this.camera.health = 100;
+    // L'armure du joueur
+    this.camera.armor = 0;
+
+    // On demande à la caméra de regarder au point zéro de la scène
+    this.camera.setTarget(BABYLON.Vector3.Zero());
+    // This attaches the camera to the canvas
+    this.camera.attachControl(canvas, true);
+
+    // Appel de la création des armes
     this.camera.weapons = new Weapons(this);
     // var hitBoxPlayer = BABYLON.Mesh.CreateBox("hitBoxPlayer", 3, scene);
     // hitBoxPlayer.parent = this.camera.playerBox;
     // hitBoxPlayer.scaling.y = 2;
     // hitBoxPlayer.isPickable = true;
     // hitBoxPlayer.isMain = true;
-    },
-    handleUserMouseDown : function() {
-        if(this.isAlive === true){
-            this.camera.weapons.fire();
-        }
-    },
-    handleUserMouseUp : function() {
-        if(this.isAlive === true){
-            this.camera.weapons.stopFire();
-        }
-    },
-    
+  },
+  handleUserMouseDown: function () {
+    if (this.isAlive === true) {
+      this.camera.weapons.fire();
+    }
+  },
+  handleUserMouseUp: function () {
+    if (this.isAlive === true) {
+      this.camera.weapons.stopFire();
+    }
+  },
 };
